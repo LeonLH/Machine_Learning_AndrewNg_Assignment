c306a4ea8661afa1e6273e8ae6bf4c26d861dbd4
LeonLH <laohan1221@126.com>
2018-08-03 08:30:39 +0800

    Finish ex2

---
+++ machine-learning-ex2/ex2/mapFeature.m
@@ -0,0 +1,21 @@
+function out = mapFeature(X1, X2)
+% MAPFEATURE Feature mapping function to polynomial features
+%
+%   MAPFEATURE(X1, X2) maps the two input features
+%   to quadratic features used in the regularization exercise.
+%
+%   Returns a new feature array with more features, comprising of 
+%   X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
+%
+%   Inputs X1, X2 must be the same size
+%
+%	Because out is initialized as all-ones at first so i is from 1, Don't need to consider about the 1's factor. 
+degree = 6;
+out = ones(size(X1(:,1)));
+for i = 1:degree
+    for j = 0:i
+        out(:, end+1) = (X1.^(i-j)).*(X2.^j);
+    end
+end
+
+end
